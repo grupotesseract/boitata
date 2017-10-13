d6965d6daa181dd48d7d9b57ed348718be88059a
Renato Gomes <renatoo.agomes@gmail.com>
2017-10-12 21:28:41 -0300

    ajustes e gitignore

--- resources/views/partials/social-media-links.blade.php
+++ resources/views/partials/social-media-links.blade.php
@@ -1,5 +1,5 @@
 
-                <a href="http://fb.com/coletivoboitata"><i class="fa fa-instagram"></i></a>
-                <a href="http://fb.com/coletivoboitata"><i class="fa fa-facebook"></i></a>
-                <a href="http://fb.com/coletivoboitata"><i class="fa fa-behance"></i></a>
-                <a href="http://fb.com/coletivoboitata"><i class="fa fa-youtube"></i></a>
+<a href="http://fb.com/coletivoboitata"><i class="fa fa-instagram"></i></a>
+<a href="http://fb.com/coletivoboitata"><i class="fa fa-facebook-square"></i></a>
+<a href="http://fb.com/coletivoboitata"><i class="fa fa-behance"></i></a>
+<a href="http://fb.com/coletivoboitata"><i class="fa fa-youtube-play"></i></a>
