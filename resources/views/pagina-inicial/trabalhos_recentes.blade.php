095238cf892eb94f4a6c1f5b489ce2f5da7c6dd3
Renato Gomes <renatoo.agomes@gmail.com>
2017-10-14 02:54:21 -0300

    Commit com secao trab recentes 80%, falta bg com as gotas e animacao das gotas. PS: responsividade 320px > x > 767 meio ruim. De resto GGWP :+1+:

--- resources/views/pagina-inicial/trabalhos_recentes.blade.php
+++ resources/views/pagina-inicial/trabalhos_recentes.blade.php
@@ -4,14 +4,14 @@
         <img src="/borda-trabalhos-recentes-top.png" alt="borda superior">
     </div>
 
-    <div class="col-xs-1 col-md-2 col-lg-3 text-right">
-        <h2 class="texto-vertical col-xs-hidden">trabalhos <br> <span>recentes</span></h2>
+    <div class="col-xs-12 col-sm-1 col-md-2 col-lg-3 text-right">
+        <h2 class="texto-vertical">trabalhos <br> <span>recentes</span></h2>
     </div>
 
-    <div class="col-xs-10 col-md-8 col-lg-6">
+    <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
         @include('pagina-inicial._slider-trabalhos-recentes')
     </div>
 
-    <div class="col-xs-10 col-md-2 col-lg-3"></div>
+    <div class="col-xs-2 col-sm-1 col-md-2 col-lg-3"></div>
 
 </section>
