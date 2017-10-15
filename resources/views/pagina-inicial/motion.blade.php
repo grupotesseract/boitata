e0fcbdc103c824326ad5929735f4b8a6ea46659a
Renato Gomes <renatoo.agomes@gmail.com>
2017-10-15 01:21:51 -0200

    motion ficando dahora

--- resources/views/pagina-inicial/motion.blade.php
+++ resources/views/pagina-inicial/motion.blade.php
@@ -6,4 +6,17 @@
     </div>
 
     <h3>motion</h3>
+    <div id="container-youtube-motion" class="col-xs-12 col-md-8 text-right">
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/3J9qxFXncMg?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div id="container-texto-motion" class="col-xs-12 col-md-4 text-left">
+        <h4>Descricao pequena nonononono</h4>
+        <p class="tag-motion">#tag</p>
+        <p class="tag-motion">#tag</p>
+        <p class="tag-motion">#tag</p>
+    </div>
+
+
+
 </section>
