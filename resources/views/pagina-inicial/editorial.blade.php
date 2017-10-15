63e60603d80facb85787e9b49531faa20588be12
Renato Gomes <renatoo.agomes@gmail.com>
2017-10-15 14:40:36 -0200

    Secao editorial eajustes gerais de responsividade :+1:

--- resources/views/pagina-inicial/editorial.blade.php
+++ resources/views/pagina-inicial/editorial.blade.php
@@ -1,4 +1,29 @@
 
 <section id="secao-editorial">
-    <h3>editorial</h3>
+
+    <div class="borda-top"></div>
+
+    <div class="container-editorial">
+        <div class="col-xs-12 col-md-3"></div>
+        <div class="col-xs-12 col-md-2 container-texto">
+            <h3>editorial</h3>
+        </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="container-item-editorial">
+                <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,w_500/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="">
+                <h4>Texto do trab</h4>
+                <div class="bottom-editorial"></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="container-item-editorial">
+                <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,w_500/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="">
+                <h4>Texto do trab</h4>
+                <div class="bottom-editorial"></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-1"></div>
+        
+</div>
+
 </section>
