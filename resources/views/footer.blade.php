2a07a451527bc9f7f4de4319cfedc03e0577fe24
Evandro Carreira <evandro.carreira@gmail.com>
2017-10-18 10:28:03 -0200

    Rodapé com contato e responsividade simples

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,22 +1,10 @@
+<footer id="rodape-site" style="background-image:url('{{ asset('img/rodape.png') }}')">
 
-
-<footer>
-
-{{-- 
-<img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,w_1920/v1507773243/Rodap%C3%A9_do_site_rs23n7.png" alt="Logo Boitatá">
---}}
-
-DESIGN
-&PROJETOS
-
-</div>
-
-<div class="col-xs-12 text-right">
-
-<a href="mailto:contato@coletivoboitata.com.br">contato@coletivoboitata.com.br</a>    
-
-@include( 'partials.social-media-links' )
-
+<div class="col-xs-12 text-right" id="infos-rodape">
+    <div class="container">
+        <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>    
+        @include( 'partials.social-media-links' )
+    </div>
 </div>
 
 
