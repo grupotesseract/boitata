78f90b74a872521f91765c9a68b19a460f379159
Evandro Carreira <evandro.carreira@gmail.com>
2017-11-21 18:17:21 -0200

    Transição fade em trabalhos recentes; menu reduzido para publicação

--- resources/views/pagina-inicial/_slider-trabalhos-recentes.blade.php
+++ resources/views/pagina-inicial/_slider-trabalhos-recentes.blade.php
@@ -1,4 +1,4 @@
-  <div id="tesseract-carousel" class="carousel slide" data-ride="carousel">
+  <div id="tesseract-carousel" class="carousel slide carousel-fade" data-ride="carousel">
 
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -29,12 +29,8 @@
 
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#tesseract-carousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#tesseract-carousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
     </a>
 
   </div>
