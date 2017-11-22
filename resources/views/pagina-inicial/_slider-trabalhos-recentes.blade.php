016894dde71fde10864a8806b2e6618469d54d6f
Renato Gomes <renatoo.agomes@gmail.com>
2017-11-21 23:28:16 -0200

    acertando a secao trabalhos recentes, texto agora aparecendo, bolinhas OK e responsividade nice :+1:

--- resources/views/pagina-inicial/_slider-trabalhos-recentes.blade.php
+++ resources/views/pagina-inicial/_slider-trabalhos-recentes.blade.php
@@ -4,25 +4,28 @@
     <ol class="carousel-indicators">
       <li data-target="#tesseract-carousel" data-slide-to="0" class="active"></li>
       <li data-target="#tesseract-carousel" data-slide-to="1"></li>
+      <li data-target="#tesseract-carousel" data-slide-to="2"></li>
+      <li data-target="#tesseract-carousel" data-slide-to="3"></li>
     </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
 
       <div class="item active">
-        <img src="/dummy-trab-recente.png" alt="Imagem1">
-        <div class="carousel-caption">
-          <h3>Texto do slider?</h3>
-        </div>
-
+        <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,h_450,w_800/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="Imagem2">
+          <h3>Ambientação para a 13º Colóquio de Moda</h3>
       </div>
-  
       <div class="item">
         <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,h_450,w_800/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="Imagem2">
-        <div class="carousel-caption">
-          <h3>Texto do slider?</h3>
-        </div>
-
+          <h3>Identidade visual para a festa universitária Inter M1l Gr4u</h3>
+      </div>
+      <div class="item">
+        <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,h_450,w_800/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="Imagem2">
+          <h3>Identidade visual para EcoArt - Engenharia e Soluções Sustentáveis</h3>
+      </div>
+      <div class="item">
+        <img src="http://res.cloudinary.com/tesseract/image/upload/c_scale,g_center,h_450,w_800/v1508033622/boitata_projetos_recentes_preambulo_2.jpg" alt="Imagem2">
+          <h3>Identidade visual para festa de 17ª anos da Naumteria</h3>
       </div>
 
     </div>
--- resources/views/pagina-inicial/trabalhos_recentes.blade.php
+++ resources/views/pagina-inicial/trabalhos_recentes.blade.php
@@ -4,14 +4,12 @@
         <img src="/borda-trabalhos-recentes-top.png" alt="borda superior">
     </div>
 
-    <div class="col-xs-12 col-sm-1 col-md-2 col-lg-2">
+    <div class="col-xs-12 col-sm-1 col-md-2 col-lg-2 col-xlg-3">
         <h2 class="texto-vertical">trabalhos <br> <span>recentes</span></h2>
     </div>
 
-    <div class="col-xs-12 col-sm-10 col-md-8 col-lg-7">
+    <div class="col-xs-12 col-sm-11 col-md-10 col-lg-10 col-xlg-9">
         @include('pagina-inicial._slider-trabalhos-recentes')
     </div>
-
-    <div class="col-xs-2 col-sm-1 col-md-2 col-lg-3"></div>
 
 </section>
