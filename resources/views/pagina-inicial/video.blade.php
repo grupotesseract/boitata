805628757c2244766a6fc1ebd84ef3d58064932a
Renato Gomes <renatoo.agomes@gmail.com>
2018-02-20 23:27:38 -0300

    Commit com tentativa de video autoplay mobile e meta descriptions

--- resources/views/pagina-inicial/video.blade.php
+++ resources/views/pagina-inicial/video.blade.php
@@ -3,5 +3,6 @@
 
 <video autoplay muted playsinline>
   <source src="http://res.cloudinary.com/tesseract/video/upload/v1507440713/home_video.mp4" type="video/mp4">
+  <source src="http://res.cloudinary.com/tesseract/video/upload/v1519179755/home_video_qpqlye.ogv" type="video/ogv">
 </video>
 </section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Coletivo Boitatá - Design & Projetos</title>
+        <title>Boitatá - Design & Projetos - Ideias que tomam forma</title>
 
-        <meta name="description" content="">
+        <meta name="description" content="Você tem uma ideia e não sabe por onde começar? Vem falar com a gente.  Consultoria - Criação de marca - Cartão de visitas e papelaria em geral - Ilustração - Motion - Site - Aplicativos - Instalações.">
         <link rel="canonical" href="https://coletivoboitata.com.br" />
 
-        <meta property="og:title" content="Coletivo Boitatá - Design & Projetos"/>
+        <meta property="og:title" content="Boitatá - Design & Projetos - Ideias que tomam forma"/>
         <meta property="og:image" content="https://coletivoboitata.com.br/og_image_boitata.png"/>
         <meta property="og:url" content="https://coletivoboitata.com.br"/>
-        <meta property="og:description" content=""/>
+        <meta property="og:description" content="Você tem uma ideia e não sabe por onde começar? Vem falar com a gente.  Consultoria - Criação de marca - Cartão de visitas e papelaria em geral - Ilustração - Motion - Site - Aplicativos - Instalações."/> 
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}"></script>
