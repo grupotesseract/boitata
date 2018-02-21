7476f0bdd9ecf0a84f953ac1dc33877318aee92f
Renato Gomes <renatoo.agomes@gmail.com>
2018-02-20 22:28:31 -0300

    Commit com ajustes pedidos para publicar a v1

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,10 +1,20 @@
-<footer id="rodape-site" style="background-image:url('{{ asset('img/rodape.png') }}')">
+<footer id="rodape-site" >
 
-<div class="col-xs-12 text-right" id="infos-rodape">
-    <div class="container">
-        <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>    
-        <div class="wrapper-social-links">
-            @include( 'partials.social-media-links' )
+<div class="col-xs-12" id="infos-rodape">
+    <div class="container container-footer">
+        <img class="logo-rodape" src="/logo_completo.png" alt="Boitatá - Design e Projetos">
+
+        <div class="container-texto-rodape desktop">
+            <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>    
+            <div class="wrapper-social-links">
+                @include( 'partials.social-media-links' )
+            </div>
+        </div>
+        <div class="container-texto-rodape mobile">
+            <div class="wrapper-social-links">
+                @include( 'partials.social-media-links' )
+            </div>
+            <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>    
         </div>
     </div>
 </div>
--- resources/views/pagina-inicial/editorial.blade.php
+++ resources/views/pagina-inicial/editorial.blade.php
@@ -10,17 +10,17 @@
         </div>
         <div class="col-xs-12 col-md-3">
             <div class="container-item-editorial text-center">
-                <a href="#">
-                    <img src="/dummy-editorial-com-borda.png" alt="">
-                    <h4>Descricao pequena nonononono nononon nononono</h4>
+                <a href="https://www.behance.net/gallery/49903177/Bicho-Transparente" target="_blank">
+                    <img src="/foto_editorial_01.png" alt="Folder impresso para espetáculo de teatro" >
+                    <h4>Folder impresso para espetáculo de teatro</h4>
                 </a>
             </div>
         </div>
         <div class="col-xs-12 col-md-3">
             <div class="container-item-editorial text-center">
-                <a href="#">
-                    <img src="/dummy-editorial-com-borda.png" alt="">
-                    <h4>Descricao pequena nonononono nononon nononono</h4>
+                <a href="https://www.behance.net/gallery/59283893/Livro-impresso-para-dissertacao-em-Design" target="_blank">
+                    <img src="/foto_editorial_02.png" alt="Livro impresso para dissertação">
+                    <h4>Livro impresso para dissertação</h4>
                 </a>
             </div>
         </div>
