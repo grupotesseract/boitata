433f3d9f32ca9aeeaf14e3c043cd7dd65f1a7325
Renato Gomes <renatoo.agomes@gmail.com>
2018-06-28 01:24:45 -0300

    acertando secao editorial para ser editavel

--- resources/views/editorials/index.blade.php
+++ resources/views/editorials/index.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
+@endsection
+
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">Editorials</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('editorials.create') !!}">Add New</a>
-        </h1>
+        <h1>Seção Editorial</h1>
     </section>
-    <div class="content">
-        <div class="clearfix"></div>
-
+    <div class="content" style="min-height:800px">
         @include('flash::message')
-
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('editorials.table')
+                <div class="col-xs-12">
+                    @include('editorials.view_editorial')
+                </div>
             </div>
         </div>
     </div>
---
+++ resources/views/editorials/view_editorial.blade.php
@@ -0,0 +1,40 @@
+
+<section id="secao-editorial">
+
+    <div class="borda-top"></div>
+
+    <div class="container-editorial">
+        <div class="col-xs-12 col-md-2"></div>
+        <div class="col-xs-12 col-md-2 container-texto">
+            <h3>editorial</h3>
+            <a class="btn btn-default" href="/editorials/1/edit">
+                <i class="fa fa-pencil"></i> Trocar
+            </a>
+        </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="container-item-editorial text-center">
+                <a href="https://www.behance.net/gallery/49903177/Bicho-Transparente" target="_blank">
+                    <img src="/foto_editorial_01.png" alt="Folder impresso para espetáculo de teatro" >
+                    <h4>Folder impresso para espetáculo de teatro</h4>
+                </a>
+                <br>
+                <a class="btn btn-default" href="/editorials/1/edit">
+                    <i class="fa fa-pencil"></i> Trocar
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="container-item-editorial text-center">
+                <a href="https://www.behance.net/gallery/59283893/Livro-impresso-para-dissertacao-em-Design" target="_blank">
+                    <img src="/foto_editorial_02.png" alt="Livro impresso para dissertação">
+                    <h4>Livro impresso para dissertação</h4>
+                </a>
+                <br>
+                <a class="btn btn-default" href="/editorials/2/edit">
+                    <i class="fa fa-pencil"></i> Trocar
+                </a>
+            </div>
+        </div>
+</div>
+
+</section>
