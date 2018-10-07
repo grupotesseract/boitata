639bd70376c6c54563a34fc345afd20abe61a11b
Thiago Braga <thibraga06@gmail.com>
2018-10-06 23:44:52 -0300

    Desativando notificações do webpack.mix.js
    No Ubuntu, a cada build do Laravel Mix, é disparado uma notificação
    para o sistema, que não é possível desativar nas configurações. Procurei
    no Google e achei no Laracasts o código para desativas as notificações.
    Funcionou perfeitamente.
    
    Também teve umas modificações no mix-manifest.json que não fui eu que
    alterei, penso que foi feito automaticamente.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,7 @@
 const { mix } = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.disableNotifications()
+    .js('resources/assets/js/app.js', 'public/js')
     .styles('resources/assets/css/dropzone.css', 'public/css/dropzone.css')
     .styles('node_modules/summernote/dist/summernote.css', 'public/css/summernote.css')
     .copy('node_modules/summernote/dist/summernote.js', 'public/js/summernote.js')
