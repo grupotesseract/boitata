9bbb82be690548ed305c134faf1a9a7c38a5c231
Thiago Braga <thibraga06@gmail.com>
2018-10-06 23:56:17 -0300

    Conteúdo da página Quem Somos (em desenvolvimento)
    Inclusão de boa parte do conteúdo da página Quem Somos, porém, sem as
    imagens e alinhamentos perfeitos de acordo com o layout enviado pelo
    Boitatá.
    
    Ainda irei trabalhar mais uns dias para deixar o resultado final dessa
    página bem satisfatório, com as fontes e alinhamentos corretos, textos
    e imagens inseridas, etc.

--- resources/views/quem-somos/show.blade.php
+++ resources/views/quem-somos/show.blade.php
@@ -1,7 +1,114 @@
-@extends ('master')
+@extends('master')
 
-@section ('content')
+@section('content')
 
+    <section class="quem-somos">
+        <div class="quem-somos-header"></div>
 
+        <div class="quem-somos-info">
+            <div class="container">
+                <h1>Quem Somos</h1>
+
+                <h2>
+                    Acima de tudo,<br>
+                    Nós somos um coletivo.
+                </h2>
+
+                <p>
+                    O Boitatá surgiu em 2012 quando dois amigos, <strong>CAROL</strong> e <strong>THOM</strong>
+                    - na época, estudantes de design -, começaram a perceber que trabalhar juntos era muito mais legal do que enfrentar a vida
+                    de freelancers sozinhos.
+                </p>
+
+                <p>
+                    E assim foi durante alguns anos, de dia o trabalho com carteira assinada e de
+                    noite, reuniões e mais reuniões madrugadas à dentro. Além dos 2 amigos, passaram
+                    pelo Boitatá outros(as) designers que foram muito importantes para que pudéssemos
+                    nos estruturar da forma como somos hoje: uma empresa que tem como principais valores
+                    o trabalho coletivo e a criatividade.
+                </p>
+
+                <p>
+                    Nossa atuação passa pelo design gráfico impresso, atravessa as redes sociais, ganha
+                    vida por meio de vídeos, animações, sites, aplicativos e se expande ainda mais ao
+                    mesclar diferentes tecnologias e materiais por meio de projetos de cenografias,
+                    ambientações e instalações.
+                </p>
+
+                <p>
+                    Nós acreditamos que o design, além de ser uma ferramenta para solucionar diferentes
+                    tipos de problemas, é acima de todas as suas funções, uma forma de ver e sentir o
+                    mundo. Uma forma de tradução.
+                </p>
+
+                <hr>
+
+                <h3 class="quotes">
+                    Esse é o Boitatá, uma mistura doida de<br>
+                    várias coisas resultantes da soma de ideias<br>
+                    contrastante de dois designers inquietos.
+                </h3>
+            </div>
+        </div>
+
+        <div class="quem-somos-fotos">
+            <div class="container">
+                <div class="col-xs-6">
+                    <img src="https://via.placeholder.com/340x340" class="img-circle" />
+
+                    <h4>Caroline Gomes</h4>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dolore hic enim commodi corrupti sint molestiae, quasi
+                        totam nisi? Cumque laborum fuga odit veniam quod corporis
+                        tempora ducimus totam magni exercitationem.
+                    </p>
+                </div>
+                <div class="col-xs-6">
+                    <img src="https://via.placeholder.com/340x340" class="img-circle" />
+
+                    <h4>Thomas Musmann</h4>
+
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Repellendus illum suscipit nam aut consequuntur deleniti
+                        possimus corrupti minima pariatur obcaecati quisquam odio,
+                        rerum perferendis numquam. Excepturi incidunt ipsam blanditiis nihil.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="quem-somos-tarefas">
+            <h2>O que fazemos</h2>
+
+            <div class="col-xs-2">
+                Identidade Visual<br>
+
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Rem sint, impedit numquam, earum nam necessitatibus incidunt
+                quasi repellat itaque sed quos, modi beatae doloribus
+                aliquam architecto quaerat porro debitis ex?
+            </div>
+
+            <div class="col-xs-2">Editorial</div>
+            <div class="col-xs-2">Ilustração</div>
+            <div class="col-xs-2">Posters</div>
+            <div class="col-xs-2">Motion</div>
+            <div class="col-xs-2">Webdesign</div>
+            <div class="col-xs-2">Instalações</div>
+        </div>
+
+        <div class="quem-somos-parcerias">
+            <h2>Parcerias</h2>
+
+            <img src="" alt="Grupo Tesseract" />
+            <img src="" alt="Oficina Desmanche" />
+            <img src="" alt="Protótipo Tópico" />
+            <img src="" alt="Zame" />
+            <img src="" alt="Eduardo Kenji Fotógrafo" />
+        </div>
+    </section>
 
 @endsection
