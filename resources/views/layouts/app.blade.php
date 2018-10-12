d3ff48d25edde9d71c239c527d19f234801b880a
Thiago Braga <thibraga06@gmail.com>
2018-10-12 12:18:10 -0300

    Indentando código em app.blade.php
    Ref #18

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,71 +21,71 @@
     @if (env('APP_ENV') === 'production')
         @include('googletagmanager::script')
     @endif
-<div class="wrapper">
-    <!-- Main Header -->
-    <header class="main-header">
+    <div class="wrapper">
+        <!-- Main Header -->
+        <header class="main-header">
 
-        <!-- Logo -->
-        <a href="#" class="logo">
-            <b>Boitatá</b>
-        </a>
-
-        <!-- Header Navbar -->
-        <nav class="navbar navbar-static-top" role="navigation">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                <span class="sr-only">Toggle navigation</span>
+            <!-- Logo -->
+            <a href="#" class="logo">
+                <b>Boitatá</b>
             </a>
-            <!-- Navbar Right Menu -->
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <!-- User Account Menu -->
-                    <li class="dropdown user user-menu">
-                        <!-- Menu Toggle Button -->
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <!-- The user image in the navbar-->
-                            <img src="/favicon-96.png"
-                                 class="user-image" alt="User Image"/>
-                            <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                            <span class="hidden-xs">{!! Auth::user()->name !!}</span>
-                        </a>
-                        <ul class="dropdown-menu" style="width: 150px; min-width: 140px;">
-                            <!-- Menu Footer-->
-                            <li class="">
-                                <div class="">
-                                    <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat form-control"
-                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        Sair
-                                    </a>
-                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                                          style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
 
-    <!-- Left side column. contains the logo and sidebar -->
-@include('layouts.sidebar')
-<!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        @yield('content')
+            <!-- Header Navbar -->
+            <nav class="navbar navbar-static-top" role="navigation">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
+                <!-- Navbar Right Menu -->
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <!-- User Account Menu -->
+                        <li class="dropdown user user-menu">
+                            <!-- Menu Toggle Button -->
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <!-- The user image in the navbar-->
+                                <img src="/favicon-96.png"
+                                    class="user-image" alt="User Image"/>
+                                <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                                <span class="hidden-xs">{!! Auth::user()->name !!}</span>
+                            </a>
+                            <ul class="dropdown-menu" style="width: 150px; min-width: 140px;">
+                                <!-- Menu Footer-->
+                                <li class="">
+                                    <div class="">
+                                        <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat form-control"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            Sair
+                                        </a>
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST"
+                                            style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <!-- Left side column. contains the logo and sidebar -->
+    @include('layouts.sidebar')
+    <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+
+        <!-- Main Footer -->
+        <footer class="main-footer" style="max-height: 100px;text-align: center">
+            <strong>Feito com carinho pelo <a target="_blank" href="https://grupotesseract.com.br">Grupo Tesseract</a> &nbsp; <i class="fa fa-heart"></i> </strong>
+        </footer>
+
     </div>
 
-    <!-- Main Footer -->
-    <footer class="main-footer" style="max-height: 100px;text-align: center">
-        <strong>Feito com carinho pelo <a target="_blank" href="https://grupotesseract.com.br">Grupo Tesseract</a> &nbsp; <i class="fa fa-heart"></i> </strong>
-    </footer>
 
-</div>
-
-
-<script src="/js/app.js"></script>
+    <script src="/js/app.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
@@ -93,12 +93,12 @@
     <!-- AdminLTE App -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js"></script>
 
-@if (env('APP_ENV') === 'local')
-    <script src="http://localhost:3000/browser-sync/browser-sync-client.js?v=2.24.7"></script>
-@endif
+    @if (env('APP_ENV') === 'local')
+        <script src="http://localhost:3000/browser-sync/browser-sync-client.js?v=2.26.0"></script>
+    @endif
 
-@yield('scripts')
-@yield('js')
+    @yield('scripts')
+    @yield('js')
 
 </body>
 </html>
