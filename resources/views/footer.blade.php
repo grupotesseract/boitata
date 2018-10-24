ce2e40093670b8403aa51589f1085103f71f4c31
Thiago Braga <thibraga06@gmail.com>
2018-10-24 00:10:20 -0300

    Ajustes nos logotipos dos Parceiros
    Dispor os logotipos utilizando flexbox, pois cada logo tem uma largura
    diferente, porém, a margem entre eles é a mesma.
    
    Já configurei a responsividade desse bloco, porém, ainda não para o
    mobile.
    
    Ref #18

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,22 +1,22 @@
-<footer id="rodape-site" >
+<footer id="rodape-site">
+    <div class="row">
+        <div class="col-xs-12" id="infos-rodape">
+            <div class="container-footer">
+                <img class="logo-rodape" src="/logo_completo.png" alt="Boitatá - Design e Projetos">
 
-<div class="col-xs-12" id="infos-rodape">
-    <div class="container-footer">
-        <img class="logo-rodape" src="/logo_completo.png" alt="Boitatá - Design e Projetos">
+                <div class="container-texto-rodape desktop">
+                    <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>
+                    <div class="wrapper-social-links">
+                        @include('partials.social-media-links')
+                    </div>
+                </div>
 
-        <div class="container-texto-rodape desktop">
-            <a href="mailto:contato@coletivoboitata.com.br"><b>contato@coletivoboitata.com.br</b></a>    
-            <div class="wrapper-social-links">
-                @include( 'partials.social-media-links' , ['links' => true] )
-            </div>
-        </div>
-        <div class="container-texto-rodape mobile">
-            <div class="wrapper-social-links">
-                @include( 'partials.social-media-links' )
+                <div class="container-texto-rodape mobile">
+                    <div class="wrapper-social-links">
+                        @include('partials.social-media-links')
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
-
 </footer>
