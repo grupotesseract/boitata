65629abcce8a596dac69d73b399d051f9ac2f725
Thiago Braga <thibraga06@gmail.com>
2018-10-29 23:10:09 -0300

    Utilizando ícones do novo sprite na navbar
    Ref #18

--- resources/views/partials/social-media-links.blade.php
+++ resources/views/partials/social-media-links.blade.php
@@ -1,15 +1,15 @@
 <a href="https://www.instagram.com/coletivoboitata/" target="_blank" rel="noreferrer noopener">
-    <i class="icon-instagram"></i>
+    <i class="navbar-icon-instagram"></i>
 </a>
 
 <a href="https://www.facebook.com/coletivoboitata" target="_blank" rel="noreferrer noopener">
-    <i class="icon-facebook"></i>
+    <i class="navbar-icon-facebook"></i>
 </a>
 
 <a href="https://www.behance.net/coletivoboitata" target="_blank" rel="noreferrer noopener">
-    <i class="icon-behance"></i>
+    <i class="navbar-icon-behance"></i>
 </a>
 
 <a href="https://www.youtube.com/channel/UCY955yf9sGoJO1Dmn4R48BQ" target="_blank" rel="noreferrer noopener">
-    <i class="icon-youtube"></i>
+    <i class="navbar-icon-youtube"></i>
 </a>
